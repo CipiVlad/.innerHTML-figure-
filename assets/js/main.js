7e8df9d529ff2759c27add18d2d598b40f712b49
CipiVlad <supercoder.cipi@gmail.com>
2022-04-14 13:46:56 +0200

    innerHTML_figure

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+
+document.getElementById("fig").innerHTML = "  <figure>Fig.1</figure>";
+document.getElementById("fig2").innerHTML = "  <figure>Fig.2</figure>";
+document.getElementById("fig3").innerHTML = "  <figure>Fig.3</figure>";
